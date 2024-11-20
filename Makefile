fe61e4c2f38c3e907a936933b8d00e0c5c349aac
Miguel Angel Garcia Beltran <angelurano00@gmail.com>
2024-11-20 14:23:36 +0100

    First commit

---
+++ Makefile
@@ -0,0 +1,36 @@
+NAME = push_swap
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror $(INCLUDES)# -g3 -fsanitize=address,leak,undefined
+
+RM = rm -f
+
+LIBFT_DIR = ./libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+SRC = ./src/*.c #TODO
+OBJ = $(SRC:.c=.o)
+
+INCLUDES = -I./includes -I$(LIBFT_DIR)/includes
+
+all: $(NAME)
+
+$(NAME): $(LIBFT) $(OBJ)
+	$(CC) $(CFLAGS) $(OBJ) $(LIBFT) -o $(NAME)
+
+$(LIBFT):
+	make -C $(LIBFT_DIR) --no-print-directory
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	make -C $(LIBFT_DIR) fclean --no-print-directory
+	$(RM) $(OBJ)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re bonus
---
+++ includes/push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: migugar2 <migugar2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 21:27:43 by migugar2          #+#    #+#             */
+/*   Updated: 2024/11/20 14:19:06 by migugar2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h> // read, write
+# include <stdlib.h> // malloc, free, exit
+
+# include "libft.h"
+
+#endif
