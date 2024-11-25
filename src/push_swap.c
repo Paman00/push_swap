3167ecc3e55b2c557418ad62fafddb28a076af8b
Miguel Angel Garcia Beltran <angelurano00@gmail.com>
2024-11-25 19:27:51 +0100

    Add libft changes for stacks

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: migugar2 <migugar2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 14:13:19 by migugar2          #+#    #+#             */
-/*   Updated: 2024/11/20 14:22:52 by migugar2         ###   ########.fr       */
+/*   Updated: 2024/11/25 19:24:48 by migugar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	main(int argc, char *argv[])
 {
 	if (argc < 2)
-		return (ft_putstr_fd(0, "Error\n"), 1);
+		return (ft_putstr_fd(STDERR_FILENO, "Error\n"), 1);
 	(void)argv;
 }
