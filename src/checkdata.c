ce71933247b0fc924173ae5d2fba3dd1873d809f
Miguel Angel Garcia Beltran <angelurano00@gmail.com>
2024-11-30 10:21:02 +0100

    Add length validation for the input

--- src/checkdata.c
+++ src/checkdata.c
@@ -6,7 +6,7 @@
 /*   By: migugar2 <migugar2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 14:30:45 by migugar2          #+#    #+#             */
-/*   Updated: 2024/11/27 21:59:35 by migugar2         ###   ########.fr       */
+/*   Updated: 2024/11/30 10:23:25 by migugar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,25 @@
 int	is_number(char *str)
 {
 	size_t	i;
+	size_t	len;
 
-	if (str == NULL || *str == '\0')
-		return (0);
 	i = 0;
+	if (str == NULL || str[i] == '\0')
+		return (0);
 	while (str[i] && ft_isspace(str[i]))
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 		i++;
 	if (str[i] == '\0')
 		return (0);
+	len = 0;
 	while (str[i] && ft_isdigit(str[i]))
+	{
 		i++;
+		len++;
+	}
+	if (len == 0 || len > 10)
+		return (0);
 	while (str[i] && ft_isspace(str[i]))
 		i++;
 	if (str[i] != '\0')
@@ -34,7 +41,7 @@ int	is_number(char *str)
 	return (1);
 }
 
-int	is_valid(char *str, long long atol, t_stack *a)
+int	is_valid(char *str, long long number, t_stack *a)
 {
 	t_dlist	*node;
 
@@ -42,12 +49,12 @@ int	is_valid(char *str, long long atol, t_stack *a)
 		return (0);
 	if (!is_number(str))
 		return (0);
-	if (atol < -2147483648 || atol > 2147483647)
+	if (number < -2147483648 || number > 2147483647)
 		return (0);
 	node = a->top;
 	while (node != NULL)
 	{
-		if (((t_content *)node->content)->value == atol)
+		if (((t_content *)node->content)->value == number)
 			return (0);
 		node = node->next;
 	}
